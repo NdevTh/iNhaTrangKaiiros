c97971cfe661d61cc1eff5621fc9b7be450e402d
ct mac <cth.huynh@gmail.com>
2025-03-28 18:28:44 +0100

    changes index new

---
+++ iNhatrang.fr/main.js
@@ -0,0 +1,9 @@
+// Burger menu
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.getElementById("burger");
+    const nav = document.getElementById("nav-links").querySelector("ul");
+  
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  });
